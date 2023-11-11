642d401e3af941ca21efbc3422ea6d4a5c4ba64a
benAzouzYassin <yassinebenazouz123@gmail.com>
2023-11-12 00:19:28 +0100

    fixing small bugs

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -7,7 +7,7 @@ export default function Poster({ HandleDetail, media }) {
         <>
             <div className="poster-container" onClick={HandleDetail}>
                 {isLoadingImg && <div className="display-poster-loading">
-                    <div class="lds-dual-ring"></div>
+                    <div className="lds-dual-ring"></div>
                 </div>}
                 <img className="display-poster" style={{ width: isLoadingImg ? "0px" : "100%" }} src={posterPath} onLoad={() => setIsLoadingImg(false)} />
                 {!isLoadingImg && <h3>{media.title ? media.title : media.name}</h3>}
